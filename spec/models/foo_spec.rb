45d4f64b39baff107471ff0b5be2db1c7372020f
Jeanine Soterwood <jeanine@littleforestconsulting.com>
2017-05-24 13:32:51 -0700

    Add a spec that checks getter methods

--- spec/models/foo_spec.rb
+++ spec/models/foo_spec.rb
@@ -4,5 +4,11 @@ RSpec.describe Foo, type: :model do
       foo = create(:foo)
       expect(foo.hello_world).to eq "hello world"
     end
+
+    it "responds to title and name methods" do
+      foo = create(:foo, name: "bob", title: "Chef")
+      expect(foo.name).to eq "bob"
+      expect(foo.title).to eq "Chef"
+    end
   end
 end
