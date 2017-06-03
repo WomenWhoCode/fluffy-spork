fccb62c06210ce1ade72591e6851f30ec7f0d5e2
Jeanine Soterwood <jeanine@littleforestconsulting.com>
2017-06-03 08:49:18 -0700

    Add logfile option for logger

--- lib/MMLog.rb
+++ lib/MMLog.rb
@@ -1,6 +1,6 @@
 require 'logger'
 
-class MMLog
+class MMLog < Logger
   def self.log
     if @logger.nil?
       @logger = Logger.new STDOUT
@@ -9,4 +9,15 @@ class MMLog
     end
     @logger
   end
+
+  def format_message(severity, timestamp, progname, msg)
+    formatted_time = timestamp.strftime("%Y-%m-%d %H:%M:%S.") << timestamp.usec.to_s[0..2].rjust(3)
+    "[%s] %s\n" % [formatted_time, msg]
+  end
 end
+
+# Usage: MMLOG.info(<stuff>)
+# (or whatever level of debug info you want to use)
+logfile = File.open('log/mm.log','a')
+logfile.sync = true
+MMLOG = MMLog.new(logfile)
