1495192ab43e3895c20677755b0e4062fb8a3966
Jeanine Soterwood <jeanine@littleforestconsulting.com>
2017-06-19 10:31:59 -0700

    Add time field to events model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170610003929) do
+ActiveRecord::Schema.define(version: 20170619173125) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20170610003929) do
     t.integer "group_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "time"
   end
 
   create_table "group_stats", force: :cascade do |t|
