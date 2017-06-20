c213e1c077e106e4b415571c316b1d26618bf627
Jeanine Soterwood <jeanine@littleforestconsulting.com>
2017-06-20 11:42:42 -0700

    Remove last_stored_event_time migrations

--- db/migrate/20170619174111_add_last_event_time_stored_to_group_stats.rb
+++
@@ -1,5 +0,0 @@
-class AddLastEventTimeStoredToGroupStats < ActiveRecord::Migration[5.1]
-  def change
-    add_column :group_stats, :last_stored_event_time, :timestamp
-  end
-end
--- db/migrate/20170620164949_remove_last_stored_event_time_from_group_stats.rb
+++
@@ -1,5 +0,0 @@
-class RemoveLastStoredEventTimeFromGroupStats < ActiveRecord::Migration[5.1]
-  def change
-    remove_column :group_stats, :last_stored_event_time, :timestamp
-  end
-end
