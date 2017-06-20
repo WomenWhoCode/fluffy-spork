374bbf689366f79d97ab8f97430cc7ce1a29e3b9
Jeanine Soterwood <jeanine@littleforestconsulting.com>
2017-06-20 10:08:20 -0700

    Factory fixup

--- spec/factories/group_stats.rb
+++ spec/factories/group_stats.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :group_stat do
     group_id { Faker::Number.number(6) }
-    urlname "WomenWhoCode-Silicon-Valley"
+    urlname "Women-Who-Code-Silicon-Valley"
   end
 end
