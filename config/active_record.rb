2dbd358f4b34ed93cfe521e077402858ade35412
Jeanine Soterwood <jeanine@littleforestconsulting.com>
2017-06-21 13:10:24 -0700

    Do not use active record log file in production

--- config/active_record.rb
+++ config/active_record.rb
@@ -1,5 +1,6 @@
 require 'active_record'
-logfile = File.open('log/active_record.log','a')
-logfile.sync = true
-ActiveRecord::Base.logger = Logger.new(logfile)  
-
+if ENV["DB"] != "production"
+  logfile = File.open('log/active_record.log','a')
+  logfile.sync = true
+  ActiveRecord::Base.logger = Logger.new(logfile)
+end
