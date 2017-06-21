484993c0f7ee15c53f8e95a0bbae9a861d6812d3
Jeanine Soterwood <jeanine@littleforestconsulting.com>
2017-06-21 13:04:59 -0700

    Add group_urlname to events

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170620164949) do
+ActiveRecord::Schema.define(version: 20170621200414) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20170620164949) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.datetime "time"
+    t.string "group_urlname"
   end
 
   create_table "group_stats", force: :cascade do |t|
